1aef113452f42a848d81648d36596e1b28e87516
Stefan Uebe <stefan@vaadin.com>
2025-07-24 17:30:29 +0200

    Removed outdated (commented out) plugins. See this page for details on each plugin why they have been removed: https://www.tiny.cloud/docs/tinymce/6/6.0-release-notes-core-changes/

--- src/main/java/org/vaadin/hugerte/Plugin.java
+++ src/main/java/org/vaadin/hugerte/Plugin.java
@@ -10,37 +10,24 @@ public enum Plugin {
     CHARMAP("charmap"),
     CODE("code"),
     CODESAMPLE("codesample"),
-//    COLORPICKER("colorpicker"), // removed from hugerte ???
-//    CONTEXTMENU("contextmenu"),  // removed from hugerte ???
     DIRECTIONALITY("directionality"),
     EMOTICONS("emoticons"),
-//    FULLPAGE("fullpage"),  // removed from hugerte ???
     FULLSCREEN("fullscreen"),
     HELP("help"),
-//    HR("hr"),  // removed from hugerte ???
     IMAGE("image"),
-//    IMAGE_TOOLS("imagetools"), // removed from hugerte ???
     IMPORT_CSS("importcss"),
     INSERT_DATETIME("insertdatetime"),
-//    LEGACYOUTPUT("legacyoutput"),  // removed from hugerte ???
     LINK("link"),
     LISTS("lists"),
     MEDIA("media"),
     NONBREAKING("nonbreaking"),
-//    NONEDITABLE("noneditable"), // removed from hugerte ???
     PAGEBREAK("pagebreak"),
-//    PASTE("paste"),  // removed from hugerte ???
     PREVIEW("preview"),
-//    PRINT("print"),  // removed from hugerte ???
     QUICKBARS("quickbars"),
     SAVE("save"),
     SEARCH_REPLACE("searchreplace"),
-//    SPELLCHECKER("spellchecker"),  // removed from hugerte ???
-//    TABFOCUS("tabfocus"),  // removed from hugerte ???
     TABLE("table"),
     TEMPLATE("template"),
-//    TEXT_COLOR("textcolor"),  // removed from hugerte ???
-//    TEXT_PATTERN("textpattern"),  // removed from hugerte ???
     VISUAL_BLOCKS("visualblocks"),
     VISUAL_CHARS("visualchars"),
     WORDCOUNT("wordcount");
