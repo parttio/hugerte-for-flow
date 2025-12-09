f174e2e0f3b218e62d49746d87877b0d9a24c4a1
Matti Tahvonen <matti@vaadin.com>
2025-12-09 09:39:28 +0200

    Tuning example

--- src/test/java/org/vaadin/hugerte/DetachInvisible.java
+++ src/test/java/org/vaadin/hugerte/DetachInvisible.java
@@ -5,6 +5,8 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.Route;
 
+import java.time.LocalTime;
+
 @Route
 @Menu
 public class DetachInvisible extends VerticalLayout {
@@ -15,6 +17,13 @@ public class DetachInvisible extends VerticalLayout {
         hugeRte.setWidthFull();
         add(hugeRte);
         add(new Button("Toggle visible", e-> hugeRte.setVisible(!hugeRte.isVisible())));
-        add(new Button("Detach", e-> hugeRte.removeFromParent()));
+        add(new Button("Toggle attached", e-> {
+            if(hugeRte.isAttached()) {
+                hugeRte.removeFromParent();
+            } else {
+                addComponentAsFirst(hugeRte);
+            }
+        }));
+        add(new Button("Set value", e-> hugeRte.setValue("Now: " + LocalTime.now())));
     }
 }
