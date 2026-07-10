d0c19c35ed341b7d0e97c05427df3a126b26126f
Matti Tahvonen <matti@vaadin.com>
2026-07-10 12:01:41 +0300

    aura support based on https://github.com/parttio/tinymce-for-flow/pull/46/changes

--- src/test/java/org/vaadin/hugerte/Application.java
+++ src/test/java/org/vaadin/hugerte/Application.java
@@ -1,6 +1,7 @@
 package org.vaadin.hugerte;
 
 import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.theme.aura.Aura;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,7 +13,8 @@ import com.vaadin.flow.component.page.Push;
  */
 @SpringBootApplication
 @Push
-@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet(Aura.STYLESHEET)
+//@StyleSheet(Lumo.STYLESHEET)
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
